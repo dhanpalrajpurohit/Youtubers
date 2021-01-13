62f1a7ae38be98e5499610388e01f18cd92d797c
Dhanpal singh Rajpurohit <rajpurohitdhanpal@gmail.com>
2021-01-13 21:03:49 +0530

    updated

--- youtubers/views.py
+++ youtubers/views.py
@@ -12,13 +12,41 @@ def youtubers(request):
     return render(request,'youtubers/youtubers.html',data)
 
 def search(request):
+    youtubers = Youtuber.objects.order_by('-created_date')
+    city_search = Youtuber.objects.values_list('city',flat=True).distinct()
+    camera_type_search = Youtuber.objects.values_list('camera_type',flat=True).distinct()
+    category_type_search = Youtuber.objects.values_list('category',flat=True).distinct()
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
-            tubers = tubers.objects(description__icontains=keyword)
+            # tubers_name_search = Youtuber.objects.filter(name__icontains=keyword)
+            # print(tubers_name_search)
+            # #tubers = tubers.objects(description__icontains=keyword)
+            youtubers = Youtuber.objects.filter(description__icontains=keyword)
+            # youtubers = youtubers.union(tubers_name_search,tubers_description)
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            youtubers = Youtuber.objects.filter(city__iexact=city)
+
+    if 'camera_type' in request.GET:
+        camera_type = request.GET['camera_type']
+        if camera_type:
+            youtubers = Youtuber.objects.filter(camera_type__iexact=camera_type)
+
+    if 'category_type' in request.GET:
+        category_type = request.GET['category_type']
+        if category_type:
+            youtubers = Youtuber.objects.filter(category_type__iexact=category_type)
+
 
     data = {
-        'tubers':tubers
+        'youtubers':youtubers,
+        'city_search':city_search,
+        'camera_type_search':camera_type_search,
+        'category_type_search':category_type_search
     }
     return render(request,'youtubers/search.html',data)
 
